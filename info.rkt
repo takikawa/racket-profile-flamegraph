3d232e1f4846d2f3f0d0dfc0615fd696721530c2
Asumu Takikawa <asumu@simplyrobot.org>
2016-12-16 08:03:42 -0800

    Update package deps

--- info.rkt
+++ info.rkt
@@ -2,5 +2,5 @@
 
 (define collection "profile-flame-graph")
 (define scribblings '(("profile-flame-graph.scrbl")))
-(define deps '("base" "rsvg" "pict" "profile-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "profile-doc"))
+(define deps '("base" "rsvg" "pict" "profile-lib" "net-lib" "data-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "profile-doc" "net-doc"))
